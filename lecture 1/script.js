32cf3b1b44f863bff226a10077fd3967cbb5a91c
pandurang jawle <jawlepandurang29@gmail.com>
2024-06-14 23:02:15 +0530

    Add lecture 1 materials

---
+++ lecture 1/script.js
@@ -0,0 +1,54 @@
+//var key word
+
+
+//Goloble key word
+// var age=25;
+
+// if(true){
+//     console.log(age);
+
+
+// }
+
+
+
+//function key word
+// function solve(){
+//     var age=25;
+//     console.log(age);
+// }
+
+// solve();
+
+// var x=10;
+// var x=20;
+
+
+//blocked code key word
+
+// {
+//     let a=10;
+//     console.log(a);
+// }
+
+
+//dynamically typed language
+
+// let a=10;
+// a="hello";
+
+// a=true;
+// a=null;
+
+// console.log(a);
+
+
+
+//Const only one time declare ata a time
+
+const a=20;
+console.log(a);
+
+// const a=24;
+// console.log(a);
+
