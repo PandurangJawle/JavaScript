e915ed786134a6c0180ce1e5c2cd57f943e1740d
pandurang jawle <jawlepandurang29@gmail.com>
2024-06-15 00:41:14 +0530

    Lecture 2 add

---
+++ lecture 2/script.js
@@ -0,0 +1,127 @@
+// Operators
+
+//arithmatic operator
+
+// let a=10;
+// let b=5;
+
+// console.log(a+b);
+// console.log(a-b);
+// console.log(a*b);
+// console.log(a/b);
+// console.log(a**b);
+
+
+
+//unary operator
+
+// let a=4;
+// console.log(++a);
+// console.log(--a);
+
+
+
+//Assigment Operator
+
+// let a=2;
+// let b=4;
+
+// a+=10;
+// console.log(a)
+
+// a=a-1;
+// console.log(a)
+
+
+
+//Comparison Operator
+
+// console.log(20>24)
+// console.log(39<66)
+// console.log(20==20)
+
+
+
+
+// ternary Operator
+
+// let age =25;
+// let status1 =(age>18) ? 'i can vote ' : 'i cannot vote ';
+
+// console.log(status1)
+
+
+
+//Logical Operator
+
+// let ans=(true && true && true)
+// console.log(ans)
+
+// let ans=(false || false || true)
+// console.log(ans)
+
+// let ans=(false || false || false)
+// console.log(ans)
+
+// let ans=!(false )
+// console.log(ans)
+
+
+
+
+
+
+
+
+
+//Conditional Statements
+
+// let age=54;
+
+// if (age>=18) {
+//     console.log('i can vote')
+    
+// } else {
+//  console.log('i cannot vote')   
+// }
+
+// let num=5;
+
+// if(num == 1){
+//     console.log('A')
+// }
+// else if(num==2){
+//     console.log('B')
+// }
+// else if(num==3){
+//     console.log('C')
+// }
+// else if(num==4){
+//     console.log('D')
+// }
+// else if(num==5){
+//     console.log('D')
+// }
+// else{
+//     console.log('F')
+//}
+
+
+
+
+//Switch Case
+
+let num=3;
+
+switch(num){
+    case 1:console.log('A')
+    break;
+    case 2:console.log('B')
+    break;
+    case 3:console.log('C')
+    break;
+    case 4:console.log('D')
+    break;
+    default: console.log('f')
+    
+}
