35d165c29d676b6e3ce8bad154f2012ddd4d56ea
pandurang jawle <jawlepandurang29@gmail.com>
2024-06-15 22:49:14 +0530

    Add initial lecture 3 files

---
+++ lecture 3/script.js
@@ -0,0 +1,69 @@
+// for loop
+// let i;
+// for( i=1;i<=10;i++){
+//    console.log("Hello")
+// }
+
+// for(let i=1;i<=10;i++)
+//     {
+//         console.log(i)
+//     }
+
+// for(let i=10;i>0;i--)
+//     {
+//         console.log(i)
+//     }
+
+// for(let i=1;i<=6;i++)
+//     {
+//         if(i==4)
+//             break;
+//         else
+//         console.log(i)
+            
+//     }
+
+
+
+//while loop
+// let i=1;
+
+// while(i<=5)
+//     {
+//        console.log(i);
+//        i++; 
+//     }
+
+// let i=1;
+// while(i<6)
+//     {
+//         if(i==3)
+//             {i++
+//             break;
+//             }
+//             else
+//         console.log(i)
+//     i++
+//     }
+
+//do-while loop
+// let i=1;
+// do{
+//     console.log(i)
+//     i++;
+// }while(i<=10)
+
+
+//String
+
+// let op1='English';
+// let op2='Hindi';
+
+// let ans=`${op1} ${op2}`;
+// console.log(ans);
+
+let op1='Hindi'
+console.log(op1.length)
+console.log(op1.toUpperCase())
+
+
