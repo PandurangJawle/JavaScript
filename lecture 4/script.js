6f5839fe3ff6c79685d7b73a170546a57095074e
pandurang jawle <jawlepandurang29@gmail.com>
2024-06-16 22:05:13 +0530

    lecture 4 add

---
+++ lecture 4/script.js
@@ -0,0 +1,45 @@
+// function defination
+
+// function syMyName(){
+//     console.log("Hello");
+// }
+
+// //function call
+// syMyName();
+
+// function PrintCounting(){
+//     for(let i=1; i<=100;i++){
+//         console.log(i);
+//     }
+// }
+
+// PrintCounting();
+
+
+//Addition
+// function add(a,b){
+//     sum=a+b;
+//     console.log(sum);
+// }
+// add(3,4);
+
+
+//return function
+
+// function add(a,b)
+// {
+//         let sum=a+b;
+//         return sum;
+// }
+
+// let ans =add(1,4);
+// console.log(ans)
+
+
+// Arrow function
+
+let getExp=(x,y)=>{
+    let ans=x+y;
+    return ans;
+}
+console.log(getExp(2,3));
