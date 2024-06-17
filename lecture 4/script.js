8f3f7393aca97507938285252a0a389fd8a564ea
pandurang jawle <jawlepandurang29@gmail.com>
2024-06-17 23:09:25 +0530

    modified

--- lecture 4/script.js
+++ lecture 4/script.js
@@ -38,8 +38,8 @@
 
 // Arrow function
 
-let getExp=(x,y)=>{
-    let ans=x+y;
-    return ans;
-}
-console.log(getExp(2,3));
+// let getExp=(x,y)=>{
+//     let ans=x+y;
+//     return ans;
+// }
+// console.log(getExp(2,3));
