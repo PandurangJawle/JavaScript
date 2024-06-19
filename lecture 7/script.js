fe96355cdb6cf1f4474237c87893918cf4f48f5f
pandurang jawle <jawlepandurang29@gmail.com>
2024-06-19 20:18:25 +0530

    lecture 7 add

---
+++ lecture 7/script.js
@@ -0,0 +1,61 @@
+
+
+function sayName(myName="Atharv"){
+    console.log("may name is:",myName);
+}
+
+sayName("Aarav");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// class Human
+// {
+//     //properties
+//     age;//public
+//     #wt=80;//private
+//     ht=180;
+
+//     constructor(newAge,newHeight,newWeight){
+//       this.age=newAge;
+//       this.ht=newHeight; 
+//       this.#wt=newWeight; 
+//     }
+
+//     //behaviour
+    
+//     walking(){
+//         console.log("I am Walking", this.#wt)
+//     }
+
+//     running(){
+//         console.log("I am Running")
+//     }
+
+//     get fetchWeight(){
+//     return this.#wt;
+// }
+
+// set modifyWeight(val){
+//     this.#wt=val;
+// }
+// }
+
+// let obj = new Human(50,170,40);
+// console.log(obj.fetchWeight);
+
+// obj.walking();
+
+
