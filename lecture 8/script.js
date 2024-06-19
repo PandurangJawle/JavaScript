60ea394fcfa6fca9b45f84752e5c0be0d98b104a
pandurang jawle <jawlepandurang29@gmail.com>
2024-06-20 00:11:29 +0530

    lecture 8 add

---
+++ lecture 8/script.js
@@ -0,0 +1,70 @@
+//built-in objects
+
+
+// // 1] math object
+// console.log(Math.PI);
+
+// console.log(Math.max(23,45,67,32,12,465));
+
+// console.log(Math.min(23,45,67,32,12,465));
+
+// console.log(Math.round(1.6));
+
+// console.log(Math.floor(4));
+
+// console.log(Math.ceil(1.1));
+
+// console.log(Math.abs(5));
+
+// console.log(Math.random());
+
+// console.log(Math.sqrt(5));
+
+// console.log(Math.pow(2,10));
+
+
+
+// // 2] Date objecr
+// let curr =new Date();
+
+// let date=new Date('June 20 1998 07:15')
+// console.log(curr)
+
+
+
+
+
+// // 3] dynamic neture object
+// let obj ={
+//     age:12,
+//     wt:94,
+//     ht:180
+// }
+
+// console.log(obj)
+// obj.color='white';
+
+// console.log(obj)
+
+
+
+
+// // 4] object cloning
+// let src ={      //src=sourc
+//     age:12,
+//     wt:94,
+//     ht:180
+// }
+
+// let dest={...src};
+// console.log("src: ",src);
+// console.log("dest: ",dest);
+
+
+
+
+ 
+
+
+
+
